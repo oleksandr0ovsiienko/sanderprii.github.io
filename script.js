78d860512f2609386769542539a43a01538e0941
Sander Prii <sander.prii@vikk.ee>
2024-10-15 10:16:59 +0300

    Made webpage attractive

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ class Car {
 
 // Sample car images (you can replace these with actual image URLs)
 const sampleImages = {
+
     'Toyota': 'https://kong-proxy-intranet.toyota-europe.com/c1-images/resize/ccis/680x680/zip/ee/product-token/a0ea7f5a-107c-4ead-8934-7ef9e3179066/vehicle/ffe6ebe8-7883-4b68-99cf-ad735ab05575/padding/50,50,50,50/image-quality/70/day-exterior-04_040.png',
     'Ford': 'https://hips.hearstapps.com/hmg-prod/images/2025-ford-explorer-st-110-65ba6d640cbb3.jpg',
     'BMW': 'https://www.bmw.ee/content/dam/bmw/common/all-models/m-series/m8-coupe/2022/navigation/bmw-8series-coupe-modellfinder.png',
@@ -51,6 +52,7 @@ function displayCars(cars) {
     cars.forEach(function(car) {
         let carDiv = document.createElement('div');
         carDiv.className = 'car';
+        carDiv.setAttribute('data-brand', car.brand);
 
         let carImage = document.createElement('img');
         carImage.src = car.image;
@@ -75,6 +77,87 @@ function displayCars(cars) {
 
 displayCars(carsList);
 
+// Update menu items dynamically
+function updateMenu() {
+    let brandList = document.getElementById('brand-list');
+    brandList.innerHTML = '';
+
+    // Add "Kõik Autod" option
+    let allCarsItem = document.createElement('li');
+    let allCarsLink = document.createElement('a');
+    allCarsLink.href = '#';
+    allCarsLink.textContent = 'Kõik Autod';
+    allCarsLink.setAttribute('data-brand', 'all');
+
+    allCarsItem.appendChild(allCarsLink);
+    brandList.appendChild(allCarsItem);
+
+    // Get unique brands from carsList
+    let brands = [...new Set(carsList.map(car => car.brand))];
+
+    brands.forEach(function(brand) {
+        let listItem = document.createElement('li');
+        let link = document.createElement('a');
+        link.href = '#';
+        link.textContent = brand;
+        link.setAttribute('data-brand', brand);
+
+        listItem.appendChild(link);
+        brandList.appendChild(listItem);
+    });
+}
+
+updateMenu();
+
+// Menu toggle functionality
+let menuButton = document.getElementById('menu-button');
+let menu = document.getElementById('menu');
+let overlay = document.createElement('div');
+overlay.className = 'overlay';
+document.body.appendChild(overlay);
+
+function openMenu() {
+    menu.classList.add('open');
+    overlay.classList.add('active');
+    document.body.classList.add('menu-open');
+}
+
+function closeMenu() {
+    menu.classList.remove('open');
+    overlay.classList.remove('active');
+    document.body.classList.remove('menu-open');
+}
+
+menuButton.addEventListener('click', function() {
+    if (menu.classList.contains('open')) {
+        closeMenu();
+    } else {
+        openMenu();
+    }
+});
+
+overlay.addEventListener('click', closeMenu);
+
+// Filter cars by brand when menu item is clicked
+document.getElementById('brand-list').addEventListener('click', function(event) {
+    if (event.target.tagName === 'A') {
+        event.preventDefault();
+        let selectedBrand = event.target.getAttribute('data-brand');
+
+        if (selectedBrand === 'all') {
+            // Show all cars
+            displayCars(carsList);
+        } else {
+            // Filter cars
+            let filteredCars = carsList.filter(car => car.brand === selectedBrand);
+            displayCars(filteredCars);
+        }
+
+        closeMenu();
+    }
+});
+
+// Function to add a new car
 function addCar(event) {
     event.preventDefault();
 
@@ -94,6 +177,9 @@ function addCar(event) {
     carsList.push(newCar);
 
     displayCars(carsList);
+
+    // Update the menu with new brand if necessary
+    updateMenu();
 
     document.getElementById('add-car-form').reset();
 }
